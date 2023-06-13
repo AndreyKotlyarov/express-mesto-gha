3cdd034e270f17bcec62d5217e25ccef7672f3ac
Andrey Kotlyarov <kotlyarov.andrey01@gmail.com>
2023-06-13 17:04:03 +0900

    feature: cards routes controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,10 +1,53 @@
 const cardModel = require('../models/card');
 
-const getCards = (req, res) => {};
-const createCard = (req, res) => {};
-const deleteCard = (req, res) => {};
-const setLike = (req, res) => {};
-const deleteLike = (req, res) => {};
+const getCards = (req, res) => {
+  cardModel
+    .find({})
+    .then((cards) => res.send(cards))
+    .catch((err) => res.status(500).send({
+      message: 'Произошла ошибка',
+      err: err.message,
+      stack: err.stack,
+    }));
+};
+const createCard = (req, res) => {
+  cardModel
+    .create(req.body)
+    .then((card) => {
+      res.status(201).send(card);
+    })
+    .catch((err) => res.status(404).send({
+      message: 'Ошибка валидации',
+      err: err.message,
+      stack: err.stack,
+    }));
+};
+const deleteCard = (req, res) => {
+  cardModel
+    .findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: `${err.name}` }));
+};
+const setLike = (req, res) => {
+  cardModel
+    .findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+      { new: true },
+    )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: `${err.name}` }));
+};
+const deleteLike = (req, res) => {
+  cardModel
+    .findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } }, // убрать _id из массива
+      { new: true },
+    )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: `${err.name}` }));
+};
 
 module.exports = {
   getCards,
--- controllers/users.js
+++ controllers/users.js
@@ -4,25 +4,21 @@ const getUsers = (req, res) => {
   userModel
     .find({})
     .then((users) => res.send(users))
-    .catch((err) =>
-      res.status(500).send({
-        message: 'Произошла ошибка',
-        err: err.message,
-        stack: err.stack,
-      })
-    );
+    .catch((err) => res.status(500).send({
+      message: 'Произошла ошибка',
+      err: err.message,
+      stack: err.stack,
+    }));
 };
 const getUserById = (req, res) => {
   userModel
     .findById(req.params.userId)
     .then((user) => res.send(user))
-    .catch((err) =>
-      res.status(404).send({
-        message: 'Пользователь не найден',
-        err: err.message,
-        stack: err.stack,
-      })
-    );
+    .catch((err) => res.status(404).send({
+      message: 'Пользователь не найден',
+      err: err.message,
+      stack: err.stack,
+    }));
 };
 const createUser = (req, res) => {
   userModel
@@ -30,13 +26,11 @@ const createUser = (req, res) => {
     .then((user) => {
       res.status(201).send(user);
     })
-    .catch((err) =>
-      res.status(404).send({
-        message: 'Ошибка валидации',
-        err: err.message,
-        stack: err.stack,
-      })
-    );
+    .catch((err) => res.status(404).send({
+      message: 'Ошибка валидации',
+      err: err.message,
+      stack: err.stack,
+    }));
 };
 const updateUser = (req, res) => {
   userModel
@@ -46,36 +40,32 @@ const updateUser = (req, res) => {
         name: req.body.name,
         about: req.body.about,
       },
-      { new: true, runValidators: true }
+      { new: true, runValidators: true },
     )
     .then((user) => {
       res.status(201).send(user);
     })
-    .catch((err) =>
-      res.status(400).send({
-        message: 'Ошибка валидации',
-        err: err.message,
-        stack: err.stack,
-      })
-    );
+    .catch((err) => res.status(400).send({
+      message: 'Ошибка валидации',
+      err: err.message,
+      stack: err.stack,
+    }));
 };
 const updateAvatar = (req, res) => {
   userModel
     .findByIdAndUpdate(
       req.user._id,
       { avatar: req.body.avatar },
-      { new: true, runValidators: true }
+      { new: true, runValidators: true },
     )
     .then((user) => {
       res.status(201).send(user);
     })
-    .catch((err) =>
-      res.status(400).send({
-        message: 'Ошибка валидации',
-        err: err.message,
-        stack: err.stack,
-      })
-    );
+    .catch((err) => res.status(400).send({
+      message: 'Ошибка валидации',
+      err: err.message,
+      stack: err.stack,
+    }));
 };
 
 module.exports = {
